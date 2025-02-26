9395b3c67572e6493318ee4ffd217bb100c4e863
Saisankar99-dev <saisankarreddyputta@gmail.com>
2025-02-26 16:42:48 +0530

    Update main.tf

--- environment/Dev/main.tf
+++ environment/Dev/main.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source     = "../../Modules/vpc"
+  cidr_block = var.vpc_cidr
+  vpc_name   = "dev-vpc"
+}
+
+
+output "vpc_id" {
+  description = "VPC ID in the dev environment"
+  value       = module.vpc.vpc_id
+}
+
+
 
