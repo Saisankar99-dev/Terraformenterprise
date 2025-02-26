203b38d5c4a9df8467e492beda82bf54b78bf9d6
Saisankar99-dev <saisankarreddyputta@gmail.com>
2025-02-26 18:12:46 +0530

    Update variables.tf

--- environment/Dev/variables.tf
+++ environment/Dev/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC in dev"
+  description = "CIDR block for the VPC in devs"
   type        = string
   default     = "10.0.0.0/16"
 }
