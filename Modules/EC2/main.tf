1988767d9af7df4119e4062df36d66ca57b48187
Saisankar99-dev <saisankarreddyputta@gmail.com>
2025-05-21 16:52:40 +0530

    Update main.tf

--- Modules/EC2/main.tf
+++ Modules/EC2/main.tf
@@ -1,1 +1,31 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.environment}-ec2-sg"
+  description = "Security group for EC2 instances"
+  vpc_id      = var.vpc_id
 
+  dynamic "ingress" {
+    for_each = var.security_group_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  for_each      = var.instance_config
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
+  subnet_id     = each.value.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  tags          = merge(each.value.tags, { Environment = var.environment })
+}
